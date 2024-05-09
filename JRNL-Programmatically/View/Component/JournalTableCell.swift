06eecac24ce1c27d2d8419af289f25be64d74730
jinwoong <baseball0020@gmail.com>
2024-05-09 18:31:46 +0900

    feat: add `JournalTableCell`

---
+++ JRNL-Programmatically/View/Component/JournalTableCell.swift
@@ -0,0 +1,19 @@
+//
+//  JournalTableCell.swift
+//  JRNL-Programmatically
+//
+//  Created by jinwoong Kim on 5/9/24.
+//
+
+import UIKit
+
+final class JournalTableCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
