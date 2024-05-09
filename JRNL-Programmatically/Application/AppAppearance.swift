6853d0a8e20714f7bb55061d161df53a7af683a7
jinwoong <baseball0020@gmail.com>
2024-05-09 20:06:19 +0900

    feat: add `AppAppearance` which handles environment appearances

---
+++ JRNL-Programmatically/Application/AppAppearance.swift
@@ -0,0 +1,14 @@
+//
+//  AppAppearance.swift
+//  JRNL-Programmatically
+//
+//  Created by jinwoong Kim on 5/9/24.
+//
+
+import UIKit
+
+final class AppAppearance {
+    static func setupAppearance() {
+        UITabBar.appearance().backgroundColor = .white
+    }
+}
