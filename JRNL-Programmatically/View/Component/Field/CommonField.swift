9a03172cb96827a8e696dd28e35f0c2613fff4fe
jinwoong <baseball0020@gmail.com>
2024-05-10 23:26:49 +0900

    feat: configure `CommonField` which represents the behavior of each cell field

--- JRNL-Programmatically/View/Component/Field/CommonField.swift
+++ JRNL-Programmatically/View/Component/Field/CommonField.swift
@@ -7,7 +7,30 @@
 
 import UIKit
 
-protocol CommonField {
+protocol CommonField<CellType> {
+    associatedtype CellType: BaseTableViewCell
+    
+    var cellContentType: CellContentType { get }
+    var height: CGFloat? { get }
+    
     func register(for tableView: UITableView)
     func dequeue(for tableView: UITableView, at indexPath: IndexPath) -> UITableViewCell
+}
+
+struct CellField<CellType: BaseTableViewCell>: CommonField {
+    var cellContentType: CellContentType
+    var height: CGFloat? = nil
+    
+    func register(for tableView: UITableView) {
+        tableView.register(CellType.self, forCellReuseIdentifier: CellType.identifier)
+    }
+    
+    func dequeue(for tableView: UITableView, at indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CellType.identifier, for: indexPath) as? CellType else {
+            return UITableViewCell()
+        }
+        cell.setup(with: cellContentType)
+        
+        return cell
+    }
 }
