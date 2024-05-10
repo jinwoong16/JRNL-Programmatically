6cd42d83d423566741267f4a98ede3c9dc12ebd1
jinwoong <baseball0020@gmail.com>
2024-05-10 19:05:22 +0900

    refactor: initialize `JournalListViewController` with its view model

--- JRNL-Programmatically/View/JournalFlow/JournalListViewController.swift
+++ JRNL-Programmatically/View/JournalFlow/JournalListViewController.swift
@@ -14,6 +14,17 @@ final class JournalListViewController: UIViewController {
         
         return tableView
     }()
+    
+    private let viewModel: JournalListViewModel
+    
+    init(viewModel: JournalListViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     // MARK: - Life Cycle
     override func viewDidLoad() {
