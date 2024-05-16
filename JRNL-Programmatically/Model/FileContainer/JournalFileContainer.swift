93121c4403eb16ebdcb5d402ec12c4f972ef08ca
jinwoong <baseball0020@gmail.com>
2024-05-17 01:22:27 +0900

    refactor: change write method logic in `JournalFileContainer`

--- JRNL-Programmatically/Model/FileContainer/JournalFileContainer.swift
+++ JRNL-Programmatically/Model/FileContainer/JournalFileContainer.swift
@@ -7,21 +7,38 @@
 
 import Foundation
 
+// TODO: - Refactor this logic first
 actor JournalFileContainer: Container {
     private let fileManager: FileManager = .default
     
-    func read() async throws -> [Journal] {
+    func read() throws -> [Journal] {
         let path = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
         let fileUrl = path.appendingPathComponent("journalData.json", conformingTo: .json)
         let data = try Data(contentsOf: fileUrl)
-        return try JSONDecoder().decode([Journal].self, from: data)
+        
+        if let single = try? JSONDecoder().decode(Journal.self, from: data) {
+            return [single]
+        } else if let array = try? JSONDecoder().decode([Journal].self, from: data) {
+            return array
+        } else {
+            throw DecodingError.typeMismatch(
+                [Journal].self,
+                DecodingError.Context(
+                    codingPath: [],
+                    debugDescription: "Expected to decode Item or [Item]."
+                )
+            )
+        }
     }
     
-    func write(with element: Journal) async throws {
+    func write(with element: Journal) throws {
+        var existingData = try read()
+        existingData.append(element)
         let path = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
         try fileManager.createDirectory(at: path, withIntermediateDirectories: true)
+        
         let fileUrl = path.appendingPathComponent("journalData.json", conformingTo: .json)
-        let jsonData = try JSONEncoder().encode(element)
+        let jsonData = try JSONEncoder().encode(existingData)
         try jsonData.write(to: fileUrl)
     }
 }
