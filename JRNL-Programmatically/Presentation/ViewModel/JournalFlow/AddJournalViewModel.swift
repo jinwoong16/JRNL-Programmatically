51832a08fae35dbe93137e9b9c331fa8df7f1c1d
jinwoong <baseball0020@gmail.com>
2024-05-17 22:56:06 +0900

    refactor: conform `CLLocationManager` to `AddJournalViewModel`

--- JRNL-Programmatically/Presentation/ViewModel/JournalFlow/AddJournalViewModel.swift
+++ JRNL-Programmatically/Presentation/ViewModel/JournalFlow/AddJournalViewModel.swift
@@ -7,18 +7,57 @@
 
 import Foundation
 import Combine
+import CoreLocation
 
-final class AddJournalViewModel {
-    private let useCase: AddJournalViewUseCase
+final class AddJournalViewModel: NSObject {
+    private let useCase: any AddJournalViewUseCase
+    private let locationManager = CLLocationManager()
+    private var currentLocation: CLLocation?
     @Published private(set) var isSuccess = false
     
-    init(useCase: AddJournalViewUseCase) {
+    init(useCase: any AddJournalViewUseCase) {
         self.useCase = useCase
+        super.init()
+        
+        configureLocationManager()
     }
-
-    func save(with journal: Journal) {
+    
+    func clickSaveButton(
+        _ title: String,
+        body: String,
+        photoData: Data?
+    ) {
         useCase
-            .save(journal: journal)
+            .save(
+                journal: Journal(
+                    rating: 5,
+                    journalTitle: title,
+                    journalDescription: body,
+                    photoData: photoData,
+                    latitude: currentLocation?.coordinate.latitude,
+                    longitude: currentLocation?.coordinate.longitude
+                )
+            )
             .assign(to: &$isSuccess)
+    }
+}
+
+extension AddJournalViewModel: CLLocationManagerDelegate {
+    private func configureLocationManager() {
+        locationManager.delegate = self
+        locationManager.requestLocation()
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let currentLocation = locations.first else {
+            debugPrint(#function, "Could not specify current location.")
+            return
+        }
+        self.currentLocation = currentLocation
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        print("Failed to find user's location: \(error.localizedDescription)")
     }
 }
