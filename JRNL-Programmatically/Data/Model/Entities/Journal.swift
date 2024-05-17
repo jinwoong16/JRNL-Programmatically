45df4a9fd06934c553bc3e971f65842d94800df7
jinwoong <baseball0020@gmail.com>
2024-05-18 00:39:35 +0900

    refactor: conform `Model` to `Journal`

--- JRNL-Programmatically/Data/Model/Entities/Journal.swift
+++ JRNL-Programmatically/Data/Model/Entities/Journal.swift
@@ -7,8 +7,10 @@
 
 import UIKit
 import MapKit
+import SwiftData
 
-final class Journal: NSObject, Identifiable, Codable {
+@Model
+final class Journal: Identifiable, Codable {
     let id: UUID
     let date: Date
     let rating: Int
@@ -37,8 +39,32 @@ final class Journal: NSObject, Identifiable, Codable {
         self.latitude = latitude
         self.longitude = longitude
     }
-}
+    
+    enum CodingKeys: CodingKey {
+        case id, date, rating, journalTitle, journalDescription, photoData, latitude, longitude
     }
+    
+    required init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(UUID.self, forKey: .id)
+        date = try container.decode(Date.self, forKey: .date)
+        rating = try container.decode(Int.self, forKey: .rating)
+        journalTitle = try container.decode(String.self, forKey: .journalTitle)
+        journalDescription = try container.decode(String.self, forKey: .journalDescription)
+        photoData = try? container.decode(Data.self, forKey: .photoData)
+        latitude = try? container.decode(Double.self, forKey: .latitude)
+        longitude = try? container.decode(Double.self, forKey: .longitude)
     }
+    
+    func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(date, forKey: .date)
+        try container.encode(rating, forKey: .rating)
+        try container.encode(journalTitle, forKey: .journalTitle)
+        try container.encode(journalDescription, forKey: .journalDescription)
+        try container.encode(photoData, forKey: .photoData)
+        try container.encode(latitude, forKey: .latitude)
+        try container.encode(longitude, forKey: .longitude)
     }
 }
